cb0887514db204951338cc46f1547db959175195
Bahadir Ustabasi <oxikairos@hotmail.com>
2022-06-20 11:18:22 +0300

    slide show carousel is added in eventdetails

--- src/components/event/event.component.jsx
+++ src/components/event/event.component.jsx
@@ -4,10 +4,8 @@ import './event.styles.css'
 
 //import mu components
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
 //import axios
--- src/pages/eventdetails/eventdetails.js
+++ src/pages/eventdetails/eventdetails.js
@@ -2,11 +2,38 @@
 //import styles
 import './eventdetails.styles.css'
 
+//import mu components
+import Grid from '@mui/material/Grid';
+import Carousel from 'react-material-ui-carousel'
+import { Paper, Button } from '@mui/material'
+import Card from '@mui/material/Card';
+
+
 function EventDetails(props){
 
     return(
         <>
-            event details
+            <Grid container>
+                <Grid item xs={2}>
+
+                </Grid>
+
+                <Grid item xs={8}>
+                    <Carousel>
+                        <Paper>
+                            <img
+                            className="d-block w-100 image"
+                            src={"https://scontent.fsaw1-11.fna.fbcdn.net/v/t39.30808-6/282750118_5298105680211362_2754277236672243018_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=Dq4_FBQRvp8AX8_m0zN&_nc_ht=scontent.fsaw1-11.fna&oh=00_AT9rX9TKZHKF5l0ej0RxO2tGYiTcWoNbw-kg7fMg0IkNoA&oe=62B483C2"}
+                            alt="img not found"
+                            />
+                        </Paper>
+                    </Carousel>
+                </Grid>
+
+                <Grid item xs={2}>
+
+                </Grid>
+            </Grid>
         </>
     )
 }
